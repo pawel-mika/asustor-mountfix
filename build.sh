4dfbdc5c2da05186fd3999bc435780e011ac08fc
Pablo <ketonal80@gmail.com>
2026-04-08 20:16:23 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e # stop on error
+
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root. Please run with sudo."
+    exit 1
+fi
+
+# 0. Update version in CONTROL/config.json
+
+CONFIG_FILE="./CONTROL/config.json"
+
+if [ -f "$CONFIG_FILE" ]; then
+    # get actual
+    VERSION=$(jq -r '.general.version' "$CONFIG_FILE")
+
+    # get parts of version
+    IFS='.' read -r MAJOR MINOR BUILD REVISION <<< "$VERSION"
+    REVISION=$((REVISION + 1))
+
+    # build new version
+    NEW_VERSION="$MAJOR.$MINOR.$BUILD.$REVISION"
+    echo "Bump version: $VERSION -> $NEW_VERSION"
+
+    # write back new version to config
+    jq ".general.version = \"$NEW_VERSION\"" "$CONFIG_FILE" > "$CONFIG_FILE.tmp" && mv "$CONFIG_FILE.tmp" "$CONFIG_FILE"
+else
+    echo "File not found: $CONFIG_FILE"
+fi
+
+OUT_FOLDER="./out"
+
+# 1. Create temp folder
+TEMP_FOLDER=$(mktemp -d)
+echo "Temp folder crated: $TEMP_FOLDER"
+
+# 2. Copy required
+cp -r CONTROL "$TEMP_FOLDER/"
+cp -r webman "$TEMP_FOLDER/"
+
+# 3. Pack app
+./apkg-tools_py3.py create "$TEMP_FOLDER/" --destination "$OUT_FOLDER/"
+
+# 4. Remove temp
+rm -rf "$TEMP_FOLDER"
+echo "Temp folder removed"
